039966ac661ec080e68cfcdf21b5c79bbb94b61e
safiagharbi <safia.gharbi.pro@gmail.com>
2025-02-10 11:55:21 +0100

    Rename data.sql to init.sql to create DB, modify DB config to add tcpKeepAlive param

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -12,10 +12,19 @@ CREATE TABLE organism(
 	PRIMARY KEY(ID)
 );
 
-INSERT INTO organism (name, description,role,target, priorities) values("CNAM",
+INSERT INTO organism (name, description, role, target, priorities) values("CNAM",
 "Caisse nationale de l'assurance maladie, tête de réseau opérationnelle du régime d'assurance maladie obligatoire en France",
 "Garantir l'accès au soin|Financer le système de santé|Prévenir des risques de santé",
 "Salariés, Etudiants, Retraités, Indépendants",
 "Digitalisation(améli, dossier médical partagé, e-prescription)|Prévention(dépistages, vaccination, santé publique)|Maîtrise des dépenses(médicaments génériques, optimisation)");
+
+INSERT INTO organism (name, description, role, target, priorities) values("CPAM",
+"Caisse primaire d'assurance maladie", "Antenne locale de la CNAM, présente dans chaque département"
+"Assure le remboursement des soins aux assurés sociaux|Gère les droits des assurés(carte vitale, arrêt maladie, maternité...)|Met en place des actions de prévention et d'accompagnement des patients",
+"Assurés",
+"Appliquer les décisions|Gérer les relations avec les assurés");
+
+-- CNAM définit les règles et politiques (Organisme central)
+-- CPAM s'occupe de leur application auprès des citoyens (Structure locale)
 
 SELECT * FROM ORGANISM \G
